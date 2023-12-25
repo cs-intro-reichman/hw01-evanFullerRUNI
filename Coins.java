7de01205daa606f92359ffec865751bf37ebe339
evanFullerRUNI <evan.fuller@post.runi.ac.il>
2023-12-25 19:07:49 +0200

    git commit GenThree.java

--- Coins.java
+++ Coins.java
@@ -5,5 +5,9 @@
 public class Coins {
 	public static void main(String[] args) {
 		// Put your code here
+		int a = Integer.parseInt(args[0]);
+		int quarters = a / 25;
+		int cents = a % 25;
+		System.out.println("Use " + quarters + " quarters and " + cents + " cents");
 	}
 }
