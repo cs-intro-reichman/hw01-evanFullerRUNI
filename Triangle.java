94b0e2eb2eb70cf4de0b7f9f770ffaa5f547ef3b
evanFullerRUNI <evan.fuller@post.runi.ac.il>
2023-12-25 21:03:43 +0200

    Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -8,7 +8,8 @@ public class Triangle {
 		int a = Integer.parseInt(args[0]);
 		int b = Integer.parseInt(args[1]);
 		int c = Integer.parseInt(args[2]);
-		System.out.println(a + ", " + b + ", " + c + ": " + (((a + b) > c) & ((b + c) > a) & ((c + a) > b)));
+		boolean triangle = ((a + b) > c) & ((b + c) > a) & ((c + a) > b);
+		System.out.println(a + ", " + b + ", " + c + ": " + triangle);
 
 	}
 }
