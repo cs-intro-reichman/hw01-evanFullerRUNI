4aff66de9d040d15d6112059a85f203180f060d3
evanFullerRUNI <evan.fuller@post.runi.ac.il>
2023-12-25 19:08:36 +0200

    commit

--- GenThree.java
+++ GenThree.java
@@ -4,6 +4,16 @@
  */
 public class GenThree {
 	public static void main(String[] args) {
-		// Put your code here	
+		int min = Integer.parseInt(args[0]);
+		int max = Integer.parseInt(args[1]) - min;
+		int num1 = (int) (Math.random() * max) + min;
+		int num2 = (int) (Math.random() * max) + min;
+		int num3 = (int) (Math.random() * max) + min;
+		int minFirst = Math.min(num1, num2);
+		int minFinal = Math.min(minFirst, num3);
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num3);
+		System.out.println(minFinal);
 	}
 }
